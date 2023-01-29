b9d19970ee936c032559e9c5741d42b5e36dcc2b
Sandeep Singh <sandeep.singh@optioncircle.com>
2023-01-29 19:22:23 +0530

    EV Charging Project Setup

---
+++ evcharging/evcharging/src/main/java/com/chargingStation/EvchargingApplication.java
@@ -0,0 +1,13 @@
+package com.chargingStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvchargingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvchargingApplication.class, args);
+	}
+
+}
---
+++ evcharging/evcharging/src/test/java/com/chargingStation/EvchargingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chargingStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvchargingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
